f3b194b6944e8e116faa8798fc648af90eac6c49
yuribodo <yemotalara@gmail.com>
2024-04-09 07:57:51 -0400

    1commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <p>Texto</p>
+      </div>
+    </>
+  )
+}
+
+export default App
