8774e5174208446f5282c61054c2b15a892b3ec3
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-08-13 20:04:07 -0400

    animations at hero component

--- Front/src/Pages/Hero/Hero.jsx
+++ Front/src/Pages/Hero/Hero.jsx
@@ -1,26 +1,21 @@
-import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import { motion } from "framer-motion"
-import Navbar from '../../Components/Navbar'
+import { useState } from 'react';
+import { motion } from "framer-motion";
+import Navbar from '../../Components/Navbar';
 import { Link } from 'react-router-dom';
 
-
-const navigation = [
-  { name: 'Projetos', href: 'projetos' },
-  { name: 'Equipes', href: 'equipes' },
-  { name: 'Noticias', href: 'noticias' },
-  { name: 'Suporte', href: 'suporte' },
-]
-
 export default function Hero() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <div className="bg-white">
-      <Navbar/>
+      <Navbar />
 
-      <div className="relative isolate px-6 pt-14 lg:px-8">
+      <motion.div 
+        className="relative isolate px-6 pt-14 lg:px-8"
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true"
@@ -33,39 +28,58 @@ export default function Hero() {
             }}
           />
         </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center motion-safe:animate-bounce">
+        <motion.div 
+          className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56"
+          initial={{ scale: 0.8, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
+          <motion.div className="hidden sm:mb-8 sm:flex sm:justify-center motion-safe:animate-bounce">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
               Descubra as novidades do IFRO{' '}
               <a href="https://portal.ifro.edu.br/calama/noticias/" className="font-semibold text-green-600">
-                <span className="absolute inset-0  " aria-hidden="true" />
+                <span className="absolute inset-0 " aria-hidden="true" />
                 Leia mais <span aria-hidden="true">&rarr;</span>
               </a>
             </div>
-          </div>
+          </motion.div>
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            <motion.h1 
+              className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"
+              initial={{ opacity: 0, x: -100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1 }}
+            >
               Projetos do GPM
-            </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
+            </motion.h1>
+            <motion.p 
+              className="mt-6 text-lg leading-8 text-gray-600"
+              initial={{ opacity: 0, x: 100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1 }}
+            >
               Descubra os projetos de pesquisa realizados por nossos pesquisadores.
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6 ">
-              <Link to="/projetos" >
+            </motion.p>
+            <motion.div 
+              className="mt-10 flex items-center justify-center gap-x-6"
+              initial={{ y: 20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ delay: 0.5, duration: 1 }}
+            >
+              <Link to="/projetos">
                 <motion.div whileHover={{ scale: 1.2 }}>
                   <p className="rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                     Ver Projetos
                   </p>
                 </motion.div>
               </Link>
-              
-              
+
               <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
                 Ler mais <span aria-hidden="true">→</span>
               </a>
-            </div>
+            </motion.div>
           </div>
-        </div>
+        </motion.div>
         <div
           className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
           aria-hidden="true"
@@ -78,7 +92,7 @@ export default function Hero() {
             }}
           />
         </div>
-      </div>
+      </motion.div>
     </div>
-  )
+  );
 }
