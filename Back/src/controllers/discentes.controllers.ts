f61b2c8b0a1ccefc326557d5f8be15dccd407bcd
yuribodo <yemotalara@gmail.com>
2024-10-19 18:08:33 -0400

    refactor: make able to create a discente even if it doeesnt have a project

--- Back/src/controllers/discentes.controllers.ts
+++ Back/src/controllers/discentes.controllers.ts
@@ -26,44 +26,48 @@ export const getDiscenteById = async (req: Request, res: Response) => {
   }
 };
 
+
 export const createDiscente = async (req: Request, res: Response) => {
     const { matricula, nome, cpf, lates, date_born, projetoId, tamanho_camisa, contato, bolsista } = req.body;
-    
+  
     try {
-        const projeto = await prisma.projeto.findUnique({
-            where: { id: projetoId }
+      
+      let projeto = null;
+      if (projetoId) {
+        projeto = await prisma.projeto.findUnique({
+          where: { id: projetoId }
         });
   
         if (!projeto) {
-            return res.status(400).json({ error: 'Projeto não encontrado. Verifique o projetoId.' });
+          return res.status(400).json({ error: 'Projeto não encontrado. Verifique o projetoId.' });
         }
+      }
   
-        
-        const newDiscente = await prisma.discentes.create({
-            data: {
-                matricula,
-                nome,
-                cpf,
-                lates,
-                date_born: new Date(date_born),
-                projetoId,
-                tamanho_camisa,
-                contato,
-                bolsista,
-            },
-        });
-        res.status(201).json(newDiscente);
+      const newDiscente = await prisma.discentes.create({
+        data: {
+          matricula,
+          nome,
+          cpf,
+          lates,
+          date_born: new Date(date_born),
+          projetoId: projetoId || null, // Define projetoId como null se não for fornecido
+          tamanho_camisa,
+          contato,
+          bolsista,
+        },
+      });
+  
+      res.status(201).json(newDiscente);
   
     } catch (error) {
-        if (error instanceof Error) {
-            console.error(error.message);
-            res.status(500).json({ error: error.message });
-        } else {
-            res.status(500).json({ error: 'An unknown error occurred' });
-        }
+      if (error instanceof Error) {
+        console.error(error.message);
+        res.status(500).json({ error: error.message });
+      } else {
+        res.status(500).json({ error: 'An unknown error occurred' });
+      }
     }
-  };
-  
+  };  
   
 
 export const updateDiscente = async (req: Request, res: Response) => {
